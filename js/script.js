36bd516fb49a578fe557ca861b2eba693df8759f
ProcyonB <pelagea.nikolaevna@gmail.com>
2020-11-16 19:17:03 +0300

    3 section+mobile

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@ $(".burger").on("click", function () {
   $(".nav").toggleClass("nav_active");
   $(".burger").toggleClass("burger-active");
 });
+
+$(".slider").slick({
+  // dots: true,
+  cssEase: "ease",
+  swipeToSlide: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+// new WOW().init();
