ecd24fc056003b4db136cb65fc6126774f39abca
ProcyonB <pelagea.nikolaevna@gmail.com>
2020-11-17 04:16:36 +0300

    programms with/without

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ $(".slider").slick({
   autoplay: true,
   autoplaySpeed: 2000,
 });
+
 $(".slider-comments").slick({
   cssEase: "ease",
   swipeToSlide: true,
@@ -38,4 +39,43 @@ $("#without_slim").on("click", function () {
   <li>похудение от -3кг</li>
   <li>Самостоятельное похудение</li>
 </ul>`);
+});
+
+$("#with_weight").on("click", function () {
+  $("#weight-price").html(`1400 &#8381;`);
+  $("#weight_list").html(`<ul>
+  <li>меню, рассчитанное под твои показатели</li>
+  <li>индивидуальное сопровождение</li>
+  <li>Тренировки от меня, после которых не будут подкашиваться ноги, только бодрость и заряд энергией на весь день</li>
+  <li>плюс 4-6 кг</li>
+  <li>лайфхаки, секреты, советы по уходу и список ресторанов, где можно вкусно перекусить</li>
+  <li>продолжительность - 1 месяц</li>
+</ul>`);
+});
+$("#without_weight").on("click", function () {
+  $("#weight-price").html(`900 &#8381;`);
+  $("#weight_list").html(`<ul>
+  <li>меню, рассчитанное под твои показатели</li>
+  <li>набор веса от 2 кг</li>
+  <li>Самостоятельный набор массы</li>
+</ul>`);
+});
+
+$("#with_training").on("click", function () {
+  $("#training-price").html(`1799 &#8381;`);
+  $("#training_list").html(`<ul>
+  <li>Жиросжигающая-интенсивная</li>
+  <li>На верхнюю часть тела</li>
+  <li>На нижнюю часть тела</li>
+  <li>Растяжка</li>
+  <li>10.000 шагов дома</li>
+</ul>
+<p class="about_training">все тренировки входят в программы с сопровождением </p>`);
+});
+$("#without_training").on("click", function () {
+  $("#training-price").html(`399 &#8381;`);
+  $(
+    "#training_list"
+  ).html(`<p class="about_training">одна тренировка из пакета: Жиросжигающая-интенсивная, На верхнюю часть тела, На нижнюю часть тела,
+  Растяжка, 10.000 шагов дома </p>`);
 });
